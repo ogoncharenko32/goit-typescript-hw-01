e2589148b36a77b5bc09fb5ccb342ab671b8f3b1
ogoncharenko32 <krapniknil32@gmail.com>
2024-11-15 18:18:46 +0200

    homework done

---
+++ src/basic/3.ts
@@ -0,0 +1,7 @@
+let val1: string | number;
+
+type EnableOrDisable = 'enable' | 'disable'
+
+let val2: EnableOrDisable;
+
+
---
+++ src/generics/2.ts
@@ -0,0 +1,22 @@
+///У вас є тип AllType. Існує функція compare, яка приймає два об'єкти. Ці об'єкти містять поля AllType.
+///Ваше завдання – використовувати Pick та generics для вказівки, що поля цих параметрів належать AllType.
+///Функція compare повинна повертати AllType.
+
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
+
+function compare<T extends keyof AllType, K extends keyof AllType>(
+  top: Pick<AllType, T>,
+  bottom: Pick<AllType, K>
+): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
